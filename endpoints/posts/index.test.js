ec46149043d9c7978c6fc9de0e4a002cb02bf87b
German Tellez <gertellezv@gmail.com>
2020-10-19 00:15:22 -0500

    Testing post creation

--- endpoints/posts/index.test.js
+++ endpoints/posts/index.test.js
@@ -1,7 +1,37 @@
+const postsHandlers = require('./index')
+
 describe('Endpoints', () => {
   describe('posts', () => {
-    it.skip('should create', () => {
-      
+    it('should create', async () => {
+      const mockUsers = [
+        {id: 1},
+        {id: 2}
+      ]
+      const post = {
+        userId: 1,
+        title: "Titulo",
+        body: "Cuerpo del post"
+      }
+      const req = {
+        body: post
+      }
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        send: jest.fn()
+      }
+
+      const axios = {
+        get: jest.fn().mockResolvedValue({ data: mockUsers}),
+        post: jest.fn().mockResolvedValue( {data: {id:1000}})
+      }
+      await postsHandlers({axios}).post(req,res)
+
+      expect(res.status.mock.calls).toEqual([[201]])
+      expect(res.send.mock.calls).toEqual([[{id: 1000}]])
+      expect(axios.post.mock.calls).toEqual([['https://jsonplaceholder.typicode.com/posts', post]])
+      expect(axios.get.mock.calls).toEqual([['https://jsonplaceholder.typicode.com/users']])
+
+
     });
   });
 });
